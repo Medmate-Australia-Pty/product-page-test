dcd32f11e94b7a7172c6bd2d268ef2ad95d61d51
Dinush Jayakody <dinushjay@gmail.com>
2023-11-03 19:02:31 +1100

    product page changes

--- resources/js/views/Product.vue
+++ resources/js/views/Product.vue
@@ -1,9 +1,43 @@
 <template>
-    <div>product page here</div>
+    <div>
+        <ProductPage
+                :product="selectedProduct"
+                :image="featureImage"
+                :fullPrice="productFullPrice"
+                :discountedPrice="productDiscountedPrice"
+        />
+    </div>
 </template>
 
 <script>
-export default {
-    
-}
+    import ProductPage from '../components/ProductPage.vue';
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                selectedProduct: [],
+                featureImage: '',
+                productFullPrice: 0,
+                productDiscountedPrice: 0
+            };
+        },
+        components: {
+            ProductPage,
+        },
+        mounted() {
+            // Make a GET request to the API to fetch product data
+            axios
+                .get('/client/products/fall-limited-edition-sneakers')
+                .then((response) => {
+                    this.selectedProduct = response.data.data;
+                    this.featureImage = response.data.data.images[0];
+                    this.productFullPrice = response.data.data.price['full'];
+                    this.productDiscountedPrice = response.data.data.price['discounted'];
+                })
+                .catch((error) => {
+                    console.error('Error fetching product:', error);
+                });
+        },
+    };
 </script>
