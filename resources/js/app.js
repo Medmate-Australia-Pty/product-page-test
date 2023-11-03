d60340ceef8469b834d48ad961b8a0b8bf2c4825
Dinush Jayakody <dinushjay@gmail.com>
2023-11-03 19:01:57 +1100

    component changes

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,12 @@ require('./bootstrap')
 
 import { createApp } from 'vue'
 import router from './router'
+import ProductPage from './components/ProductPage.vue';
 
 const app = createApp({})
 
 app.use(router)
+
+app.component('product-page', ProductPage);
 
 app.mount('#app')
