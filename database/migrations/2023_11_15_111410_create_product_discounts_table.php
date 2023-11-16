056abd1e2ae18ddb63566a7a4c5481b0aef0921a
MV-J <mattvj97@gmail.com>
2023-11-16 17:34:48 +1100

    add migrations to create tables: products, product_discounts, product_images

---
+++ database/migrations/2023_11_15_111410_create_product_discounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_discounts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id');
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->enum('type', ['percent', 'amount']);
+            $table->decimal('discount', $precision = 8, $scale = 2);
+            //TODO - add bool column to track whether active
+            //TODO - add date column to track deactivation timestamp to allow future analysis
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_discounts');
+    }
+}
